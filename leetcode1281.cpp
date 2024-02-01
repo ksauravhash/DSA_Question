eb4bfe53af73b656489a95a3d6864a18cbd24347
Saurav Kumar <ksauravhash@gmail.com>
2024-02-01 19:42:58 +0530

    1281 and 191

---
+++ leetcode1281.cpp
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+ */
+
+class Solution
+{
+public:
+    int subtractProductAndSum(int n)
+    {
+        int product = 1, sum = 0;
+        while (n != 0)
+        {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+        int ans = product - sum;
+        return ans;
+    }
+};
