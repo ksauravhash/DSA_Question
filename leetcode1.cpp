e33acf42dabd557c6ca089712db8730dd685e5e2
Saurav Kumar <ksauravhash@gmail.com>
2024-02-04 18:24:59 +0530

    better solution to 1

--- leetcode1.cpp
+++ leetcode1.cpp
@@ -1,25 +1,29 @@
 /**
  *https://leetcode.com/problems/two-sum/
  */
-
 #include <vector>
+#include <unordered_map>
 
 class Solution
 {
 public:
     std::vector<int> twoSum(std::vector<int> &nums, int target)
     {
+        std::unordered_map<int, int> map;
         std::vector<int> ans;
         for (int i = 0; i < nums.size(); i++)
         {
-            for (int j = i + 1; j < nums.size(); j++)
+            int value = (target - nums[i]);
+            if (map[value] != 0)
             {
-                if (nums[i] + nums[j] == target)
-                {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
+
+                ans.push_back(i);
+                ans.push_back(map[value] - 1);
+                return ans;
+            }
+            else
+            {
+                map[nums[i]] = i + 1;
             }
         }
         return ans;
